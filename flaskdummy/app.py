ce982a10389bb76f5d0f7aee270f5b2fc255efdb
Adith Ramdas <aditramdas@gmail.com>
2023-01-14 20:23:48 +0530

    third

--- flaskdummy/app.py
+++ flaskdummy/app.py
@@ -1,11 +1,11 @@
 import os
+import docx
 from flask import Flask, request, redirect, url_for, render_template, send_from_directory
 from werkzeug.utils import secure_filename
-from PyPDF2 import PdfFileReader, PdfFileWriter
 
 UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/uploads/'
 DOWNLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/downloads/'
-ALLOWED_EXTENSIONS = {'pdf', 'txt'}
+ALLOWED_EXTENSIONS = {'pdf', 'txt', 'docx'}
 
 app = Flask(__name__, static_url_path="/static")
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
@@ -23,15 +23,22 @@ def allowed_file(filename):
 def index():
     if request.method == 'POST':
         if 'file' not in request.files:
-            print('No file attached in request')
+            print('No myr attached in request')
             return redirect(request.url)
-        file = request.files['file']
+        file = request.files['thenga']
         if file.filename == '':
-            print('No file selected')
+            print('No myr selected')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            document = docx.Document('document.docx')
+            questions = []
+            for para in document.paragraphs:
+                if para.text.startswith('Q)'):
+                    questions.append(para.text)
+
+            print(questions)
             # process_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), filename)
             # return redirect(url_for('uploaded_file', filename=filename))
     return render_template('index.html')
