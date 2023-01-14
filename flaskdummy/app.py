5edc49400f44d42dcf6d0087d3163efaab673f33
Arjun-A-I <arjunai5768@gmail.com>
2023-01-15 02:07:16 +0530

    Added_outputpdf

--- flaskdummy/app.py
+++ flaskdummy/app.py
@@ -3,6 +3,11 @@ from flask import Flask, request, redirect, url_for, render_template, send_from_
 from werkzeug.utils import secure_filename
 from PyPDF2 import PdfReader
 import openai
+import numpy as np
+# import pywhatkit as kit
+import textwrap
+from fpdf import FPDF
+import webbrowser
 
 UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/uploads/'
 DOWNLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/downloads/'
@@ -12,14 +17,37 @@ app = Flask(__name__, static_url_path="/static")
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
-# limit upload size upto 8mb
 app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024
-
-# def extract
 
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def text_to_pdf(text, filename):
+    a4_width_mm = 210
+    pt_to_mm = 0.35
+    fontsize_pt = 10
+    fontsize_mm = fontsize_pt * pt_to_mm
+    margin_bottom_mm = 10
+    character_width_mm = 7 * pt_to_mm
+    width_text = a4_width_mm / character_width_mm
+
+    pdf = FPDF(orientation='P', unit='mm', format='A4')
+    pdf.set_auto_page_break(True, margin=margin_bottom_mm)
+    pdf.add_page()
+    pdf.set_font(family='Courier', size=fontsize_pt)
+    splitted = text.split('\n')
+
+    for line in splitted:
+        lines = textwrap.wrap(line, width_text)
+
+        if len(lines) == 0:
+            pdf.ln()
+
+        for wrap in lines:
+            pdf.cell(0, fontsize_mm, wrap, ln=1)
+    pdf.output(filename, 'F')
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -39,7 +67,7 @@ def index():
             # return redirect(url_for('uploaded_file', filename=filename))
             reader = PdfReader(f"uploads/{filename}")
             page = reader.pages[0]
-            openai.api_key = "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"
+            openai.api_key = "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"
             response = openai.Completion.create(
                 model="text-davinci-003",
                 prompt="Extract questions from the given text. Write answers in about 200 words in a numbered manner. Also display the question before each answer" + page.extract_text(),
@@ -54,6 +82,25 @@ def index():
             # print(response)
 
             print(text)
+            with open('readme.txt', 'w') as f:
+                f.writelines(text)
+            input_filename = 'readme.txt'
+            output_filename = 'output.pdf'
+            file = open(input_filename)
+            text = file.read()
+            file.close()
+            text_to_pdf(text, output_filename)
+            webbrowser.open_new(r'output.pdf')
+            # kit.text_to_handwriting(text, save_to="handwriting.png")
+            # img = cv2.imread("handwriting.png")
+            # cv2.imshow("Text to Handwriting", img)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
+            # image = np.zeros((500, 500, 3), np.uint8)
+            # font = cv2.FONT_HERSHEY_SIMPLEX
+            # cv2.putText(image, text, (50, 250), font, 1, (255, 255, 255), 2)
+            # cv2.imwrite("handwriting.png", image)
+
     return render_template('index.html')
 
 
---
+++ flaskdummy/readme.txt
@@ -0,0 +1,10 @@
+
+
+1. Q) What does the fox say?
+A) The fox is a popular animal in folklore and literature, and is often associated with cunning and intelligence. In popular culture, the fox is often portrayed as saying "What does the fox say?" This phrase is used to express a sense of mystery and curiosity, and is often used as a humorous response to a difficult or confusing question.
+
+2. Q) What is the meaning of life?
+A) The meaning of life is a philosophical question that has been debated for centuries. Different people have different answers to this question, and there is no single, definitive answer. Some people believe that the meaning of life is to find happiness and fulfillment, while others believe that the meaning of life is to serve a higher purpose or to make a difference in the world. Ultimately, the meaning of life is a personal journey that each individual must discover for themselves.
+
+3. Q) Why is the earth round?
+A) The earth is round because of the force of gravity. Gravity is a force that pulls objects towards each other, and the earth is no exception. The force of gravity causes the earth to be pulled into a spherical shape, which is why it appears round from the surface. Additionally
